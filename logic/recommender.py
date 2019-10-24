14c25bc4d6f6dc66d03c78853a522694f982a085
steAz <kazanov18@wp.pl>
2019-10-24 19:48:18 +0200

    Created Recommender by copying content from State class -> some useless bullshit from State needs to be destroyed now ...

--- logic/recommender.py
+++ logic/recommender.py
@@ -7,9 +7,6 @@ It can be inherited by CollaborativeReccomender etc.
 class Recommender():
 
     def __init__(self, previousState, recommender):
-         self._previousState = previousState
-         self._iterationNo = 0
-         self._accuracies = []
-         self._reccomender = recommender
+         pass
 
     
