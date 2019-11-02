4417551fc8cbdd72c97b8c65d6e1fa5ae943da92
Katarzyna <kstrzcinska@gmail.com>
2019-11-02 17:25:23 +0100

    Second way to transform listen_counts to ratings

--- logic/datasetLoader.py
+++ logic/datasetLoader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class DatasetLoader:
 
@@ -7,8 +8,33 @@ class DatasetLoader:
         userListenCounts.columns = ['userId', 'song_id', 'listen_count']
         allUserListenCounts = userListenCounts.groupby(by = "userId")['listen_count'].sum().reset_index(name = 'all_listen_count')
         songs_ratings = pd.merge(userListenCounts, allUserListenCounts, on = 'userId')
-        songs_ratings['rating'] = ( songs_ratings['listen_count'] / songs_ratings['all_listen_count'] ) * 5
+        songs_ratings['rating'] = (( songs_ratings['listen_count'] / songs_ratings['all_listen_count'] ) * 5).apply(np.ceil)
         songs_ratings.drop(['listen_count', 'all_listen_count'], 1, inplace = True)
+        songs_ratings.to_csv("./data/songs_ratings.csv", index = False)
+
+    def prepareDataset2(self):
+        userListenCounts = pd.read_table("./data/user_songs_listen_counts.txt", header = None)
+        userListenCounts.columns = ['userId', 'song_id', 'listen_count']
+        allUserListenCounts = userListenCounts.groupby(by = "userId")['listen_count'].max().reset_index(name = 'max_listen_count')
+        songs_ratings = pd.merge(userListenCounts, allUserListenCounts, on = 'userId')
+        songs_ratings['rating'] = np.select(
+            [
+                songs_ratings['listen_count'].between(0, 0.2 * songs_ratings['max_listen_count']), 
+                songs_ratings['listen_count'].between(0.2 * songs_ratings['max_listen_count'], 0.4 * songs_ratings['max_listen_count']),
+                songs_ratings['listen_count'].between(0.4 * songs_ratings['max_listen_count'], 0.6 * songs_ratings['max_listen_count']),
+                songs_ratings['listen_count'].between(0.6 * songs_ratings['max_listen_count'], 0.8 * songs_ratings['max_listen_count']),
+                songs_ratings['listen_count'].between(0.8 * songs_ratings['max_listen_count'], songs_ratings['max_listen_count']),
+            ], 
+            [
+                1,
+                2,
+                3,
+                4,
+                5
+            ], 
+            default = 3
+        )
+        songs_ratings.drop(['listen_count', 'max_listen_count'], 1, inplace = True)
         songs_ratings.to_csv("./data/songs_ratings.csv", index = False)
 
     def loadDataset(self):
